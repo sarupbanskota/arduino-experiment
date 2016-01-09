bff0daa0f0a8057201b1a24cde1631684bd5691d
Sarup Banskota <sbanskota08@gmail.com>
2016-01-09 23:20:43 +0800

    Add working ultrasonic detector arduino file

---
+++ ultimate.ino
@@ -0,0 +1,40 @@
+/* stuff that won't change */
+const int redLight = 8;
+const int trigger = 13;
+const int echo = 12;
+
+void setup() {
+  // put your setup code here, to run once:
+  // UV dude
+  Serial.begin (9600);
+  pinMode(trigger, OUTPUT);
+  pinMode(echo, INPUT);
+  pinMode(redLight, OUTPUT);
+
+}
+
+void loop() {
+    // stuff that I don't want to bother with - for UV dude  
+    long duration, distance;
+    digitalWrite(trigger, LOW);  
+    delayMicroseconds(2); 
+    digitalWrite(trigger, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigger, LOW);
+    duration = pulseIn(echo, HIGH);
+    distance = (duration / 2) / 29.1;
+
+    if (distance < 10) { 
+      digitalWrite(redLight, HIGH); 
+      // # TODO beep here
+    }
+    if (distance >= 200 || distance <= 0) {
+      Serial.println("Out of range");
+    }
+    else {
+      Serial.print(distance);
+      Serial.println(" cm");
+    }
+  delay(500);
+
+}
